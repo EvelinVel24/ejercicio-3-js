e67b75a79fc0aec048f3b76a12670ba38834d4ac
Evelin <eveliz2001@gmail.com>
2024-08-11 17:04:28 -0400

    ajustes

--- js.js
+++ js.js
@@ -5,26 +5,26 @@ function mostrarMenu() {
         // Mostrar el menú
         opcion = prompt(
             "Seleccione una opción:\n" +
-            "1. Opción 1\n" +
-            "2. Opción 2\n" +
-            "3. Opción 3\n" +
-            "4. Opción 4\n" +
+            "1. Ver las mejores películas de todos los tiempos\n" +
+            "2. Conocer los próximos estrenos\n" +
+            "3. Obtener información sobre actores famosos\n" +
+            "4. Revisar las reseñas más recientes\n" +
             "5. Salir"
         );
 
         // Procesar la opción seleccionada
         switch (opcion) {
             case '1':
-                alert('Has seleccionado la Opción 1.');
+                alert('Las mejores películas de todos los tiempos incluyen clásicos como "El Padrino", "Casablanca", y "Titanic".');
                 break;
             case '2':
-                alert('Has seleccionado la Opción 2.');
+                alert('Próximos estrenos incluyen "The Crow", "Venom:The Last Dance", y "Joker 2".');
                 break;
             case '3':
-                alert('Has seleccionado la Opción 3.');
+                alert('Actores famosos incluyen Leonardo DiCaprio, Meryl Streep, y Denzel Washington.');
                 break;
             case '4':
-                alert('Has seleccionado la Opción 4.');
+                alert('Las reseñas más recientes están disponibles en Rotten Tomatoes y Metacritic.');
                 break;
             case '5':
                 alert('¡Saliendo del menú!');
@@ -35,3 +35,4 @@ function mostrarMenu() {
         }
     } while (opcion !== '5'); // Repetir hasta que el usuario seleccione la opción 5
 }
+
